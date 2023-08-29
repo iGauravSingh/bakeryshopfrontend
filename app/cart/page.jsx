cf9c599e3bf2bb9f854a9bcb81466c1a31e99efc
iGauravSingh <poetonroad@gmail.com>
2023-08-29 07:31:45 +0530

    cart page data to new component

--- app/cart/page.jsx
+++ app/cart/page.jsx
@@ -1,41 +1,11 @@
-"use client"
-import React, { useContext } from 'react'
+import React from 'react'
+import CartData from '../components/cartData/CartData'
 
-import { ShopContext } from '../components/contexts/ShopContext'
-
-export default function page() {
-
-    const {shop} = useContext(ShopContext)
-
-
+export default function () {
   return (
-    <div className='text-2xl px-4 md:px-[20rem] font-mono'>
-      <div>
-        <h2 className='font-bold text-2xl md:text-3xl text-center mb-12 mt-8 pb-4 border-b-2'>YOUR ITEM'S</h2>
-        <div>
-          {shop?.length === 0 ?
-          <h2 className='font-bold text-2xl text-green-700'>Nothing Here Yet continue Shoping</h2>
-          :
-          <div>
-            {shop?.map(item=> (
-              <div className='flex gap-4 mb-4 items-center justify-between' key={item.id}>
-                <p>{item.name}</p>
-                <p className='font-medium'>$ {item.price}</p>
-              </div>
-            ))}
-
-          <div className='flex justify-between items-center mt-8 font-bold text-2xl md:text-3xl border-t-2 pt-8'>
-            <p>TOTAL AMOUNT</p>
-            <p>$ 90</p>
-          </div>
-          <div className='text-center mt-8'>
-          <button className='bg-slate-950 text-white w-[30%] px-4 py-4'>CHECKOUT</button>
-          </div>
-            </div> 
-            }
-          </div>
-
-      </div>
+    <div>
+      <CartData />
     </div>
   )
 }
+
